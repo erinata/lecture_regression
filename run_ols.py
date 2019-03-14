45814aa943c9ebefc22d721456cb4bfc4f0e9927
Chungsang Tom Lam <erinata@gmail.com>
2019-03-14 17:59:33 -0400

    prediction

--- run_ols.py
+++ run_ols.py
@@ -18,7 +18,15 @@ regression = linear_model.LinearRegression()
 
 regression.fit(data, target)
 
+X = [
+	[24,55,31,3,0,7,20],
+	[40,50,2,5,1,8,20],
+	[3,95,37,3,1,15,17],
+]
 
+results = regression.predict(X)
+
+print(results)
 
 
 
